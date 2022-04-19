03144332aff92ee153f3a6fd4f1c2b40bd8c8cd7
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-04-19 22:09:32 +0900

    [백준 2741] 반복문을 사용해서 숫자찍기 (1부터 N까지)

---
+++ baekjoon/baekjoon_2741.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+/**
+ * https://www.acmicpc.net/problem/2741
+ * n 숫자 찍기 (정순)
+ * Scannner, println을 사용한 풀이
+ */
+
+fun main(arg: Array<String>) {
+    val sc = Scanner(System.`in`)
+    for (i in 1..sc.nextInt()) { // 1부터 입력값까지 반복
+        println(i) // 반복 내용 출력
+    }
+}
