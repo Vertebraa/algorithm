f3bda64b76e76acbad577e0b9ead2a1ac4bfca9c
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-05-12 21:20:01 +0900

    [백준 11651] 정렬문제 좌표 정렬하기
    - data class를 사용해서 풀이함
    - 이전 문제와 단순히 x와 y의 sort 우선 순위만 바뀜

---
+++ baekjoon/정렬/baekjoon_11651.kt
@@ -0,0 +1,29 @@
+import java.util.*
+
+/**
+ * https://www.acmicpc.net/problem/11651
+ * 정렬 문제 좌표 정렬하기 2
+ * x와 y중 y좌표를 우선 오름차순 정렬하고 y좌표가 같을 경우 x좌표를 오름차순으로 정리
+ * 리스트에 각 index에 두개의 변수를 넣기위해 data class를 사용해서 풀이함
+ */
+
+fun main(arg: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val testCase = sc.nextInt()
+    val list = mutableListOf<Coordinate>()
+
+    for (i in 0 until testCase) { // 몇개의 좌표가 들어갈 것인지 제한
+        val x = sc.nextInt() // x좌표를 입력받음
+        val y = sc.nextInt() // y좌표를 입력받음
+        val coordinate = Coordinate(x, y) // data class x값과 y값을 변수화
+        list.add(coordinate)
+    }
+
+    list.sortWith(compareBy({ it.y }, { it.x })) // y를 우선하여 sort하고 다음으로 x를 sort함
+    list.forEach { println("${it.x} ${it.y}") }
+}
+
+data class Coordinate(
+    val x: Int,
+    val y: Int
+)
