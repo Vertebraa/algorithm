84b4b3b1c8e48ed8bebe29bded99a24706fff377
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-21 14:27:30 +0900

    [백준 1008] 자바 문법연습

---
+++ baekjoon_java/baekjoon_1008_java.java
@@ -0,0 +1,16 @@
+/**
+ * https://www.acmicpc.net/problem/1008
+ * 자바 문법 연습
+ */
+
+import java.util.Scanner;
+
+public class baekjoon_1008_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a/b);
+    }
+}
