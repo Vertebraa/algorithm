636611f3837aa8820e4888c380ce95df75ee5d32
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-22 20:14:45 +0900

    [백준 10172] 자바 문법연습

---
+++ baekjoon_java/baekjoon_10172_java.java
@@ -0,0 +1,14 @@
+/**
+ * https://www.acmicpc.net/problem/10172
+ * 자바 문법 연습
+ */
+
+public class baekjoon_10172_java {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
