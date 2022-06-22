a2fb01533aafb32195cd4ea572b668299c2e7097
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-22 20:14:28 +0900

    [백준 10171] 자바 문법연습

---
+++ baekjoon_java/baekjoon_10171_java.java
@@ -0,0 +1,15 @@
+/**
+ * https://www.acmicpc.net/problem/10171
+ * 자바 문법 연습
+ * 백슬래쉬 (이스케이프 문자) 는 단독으로 사용할 수 없기 때문에 두개씩 사용해 줘야함
+ */
+
+public class baekjoon_10171_java {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
