0a928468f869566bab49d474c968cc7708f4fbef
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-24 22:36:46 +0900

    [백준 10718] 자바 문법연습

---
+++ baekjoon_java/baekjoon_10718_java.java
@@ -0,0 +1,11 @@
+/**
+ * https://www.acmicpc.net/problem/10718
+ * 자바 문법 연습
+ */
+
+public class baekjoon_10718_java {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
