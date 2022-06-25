6e3628a44d2b67752b9412ce4b98579864fbc4cc
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-25 19:52:35 +0900

    [백준 10926] 자바 문법연습

---
+++ baekjoon_java/baekjoon_10926_java.java
@@ -0,0 +1,15 @@
+/**
+ * https://www.acmicpc.net/problem/10926
+ * 자바 문법 연습
+ */
+
+import java.util.Scanner;
+
+public class baekjoon_10926_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        a += "??!";
+        System.out.print(a);
+    }
+}
