9b39424e95a6b7eacd74b2c176c41a7f1bf9087f
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-06-26 21:23:29 +0900

    [백준 2557] 자바 문법연습

---
+++ baekjoon_java/baekjoon_2557_java.java
@@ -0,0 +1,10 @@
+/**
+ * https://www.acmicpc.net/problem/2557
+ * 자바 문법 연습
+ */
+
+public class baekjoon_2557_java {
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+    }
+}
