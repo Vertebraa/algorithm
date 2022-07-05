dc985650bac7f3b2f16890ba2eb7b60c335daedf
DESKTOP-93TENLB\USER <ea_big@naver.com>
2022-07-05 23:29:40 +0900

    [백준 11022] 자바 문법연습

---
+++ baekjoon_java/반복문/baekjoon_11022_java.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baekjoon_11022_java {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 1; i < testCase + 1; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a + b) + "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
